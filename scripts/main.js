344ddfda46249ad39b9f15ae89baff54032ec664
mlla <maud@mlav.land>
2019-01-02 18:58:57 +0100

    ajout du fichier main.js

---
+++ scripts/main.js
@@ -0,0 +1,17 @@
+let monBouton = document.querySelector('button');
+
+monBouton.addEventListener('click',function()
+{
+	document.bgColor="magenta";
+});
+
+
+
+
+document.addEventListener (setTimeout(function()
+{
+	if (confirm("Hey le site xxxvidsxxx est trop bien. Veux-tu venir ?")) 
+{
+  window.location.href="https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+} 
+}, 1000));
